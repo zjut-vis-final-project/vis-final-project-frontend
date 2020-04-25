93c6a22183d7b1a78c5159e826736c5a6e88e2e1
蔡鎔玖 <874088962@qq.com>
2020-04-25 19:41:02 +0800

    绘制省级边界并加入基本交互

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -7,6 +7,7 @@
 
 <script>
 import * as L from "leaflet";
+import ChinaCoordinates from "../assets/data/ChinaCoordinates.json";
 export default {
   data() {
     return {
@@ -16,160 +17,9 @@ export default {
   },
   mounted() {
     this.loadmap();
-    // this.drawLine();
+    this.drawProvinces();
   },
   methods: {
-    // async drawLine() {
-    //   //   const data = await this.$http.get(
-    //   //     "http://echarts.baidu.com/data/asset/data/hangzhou-tracks.json"
-    //   //   );
-    //   let lines = data.map(function(track) {
-    //     return {
-    //       coords: track.map(function(seg, idx) {
-    //         return seg.coord;
-    //       })
-    //     };
-    //   });
-    //   let mychart = this.$echarts.init(document.getElementById("map"));
-    //   mychart.setOption(
-    //     (option = {
-    //       bmap: {
-    //         center: [120.13066322374, 30.240018034923],
-    //         zoom: 14,
-    //         roam: true,
-    //         mapStyle: {
-    //           styleJson: [
-    //             {
-    //               featureType: "water",
-    //               elementType: "all",
-    //               stylers: {
-    //                 color: "#d1d1d1"
-    //               }
-    //             },
-    //             {
-    //               featureType: "land",
-    //               elementType: "all",
-    //               stylers: {
-    //                 color: "#f3f3f3"
-    //               }
-    //             },
-    //             {
-    //               featureType: "railway",
-    //               elementType: "all",
-    //               stylers: {
-    //                 visibility: "off"
-    //               }
-    //             },
-    //             {
-    //               featureType: "highway",
-    //               elementType: "all",
-    //               stylers: {
-    //                 color: "#fdfdfd"
-    //               }
-    //             },
-    //             {
-    //               featureType: "highway",
-    //               elementType: "labels",
-    //               stylers: {
-    //                 visibility: "off"
-    //               }
-    //             },
-    //             {
-    //               featureType: "arterial",
-    //               elementType: "geometry",
-    //               stylers: {
-    //                 color: "#fefefe"
-    //               }
-    //             },
-    //             {
-    //               featureType: "arterial",
-    //               elementType: "geometry.fill",
-    //               stylers: {
-    //                 color: "#fefefe"
-    //               }
-    //             },
-    //             {
-    //               featureType: "poi",
-    //               elementType: "all",
-    //               stylers: {
-    //                 visibility: "off"
-    //               }
-    //             },
-    //             {
-    //               featureType: "green",
-    //               elementType: "all",
-    //               stylers: {
-    //                 visibility: "off"
-    //               }
-    //             },
-    //             {
-    //               featureType: "subway",
-    //               elementType: "all",
-    //               stylers: {
-    //                 visibility: "off"
-    //               }
-    //             },
-    //             {
-    //               featureType: "manmade",
-    //               elementType: "all",
-    //               stylers: {
-    //                 color: "#d1d1d1"
-    //               }
-    //             },
-    //             {
-    //               featureType: "local",
-    //               elementType: "all",
-    //               stylers: {
-    //                 color: "#d1d1d1"
-    //               }
-    //             },
-    //             {
-    //               featureType: "arterial",
-    //               elementType: "labels",
-    //               stylers: {
-    //                 visibility: "off"
-    //               }
-    //             },
-    //             {
-    //               featureType: "boundary",
-    //               elementType: "all",
-    //               stylers: {
-    //                 color: "#fefefe"
-    //               }
-    //             },
-    //             {
-    //               featureType: "building",
-    //               elementType: "all",
-    //               stylers: {
-    //                 color: "#d1d1d1"
-    //               }
-    //             },
-    //             {
-    //               featureType: "label",
-    //               elementType: "labels.text.fill",
-    //               stylers: {
-    //                 color: "#999999"
-    //               }
-    //             }
-    //           ]
-    //         }
-    //       },
-    //       series: [
-    //         {
-    //           type: "lines",
-    //           coordinateSystem: "bmap",
-    //           data: lines,
-    //           polyline: true,
-    //           lineStyle: {
-    //             color: "purple",
-    //             opacity: 0.6,
-    //             width: 1
-    //           }
-    //         }
-    //       ]
-    //     })
-    //   );
-    // },
     loadmap() {
       this.map = L.map("map", {
         minZoom: 3,
@@ -188,6 +38,20 @@ export default {
           attribution: "高德"
         }
       ).addTo(this.map);
+    },
+    drawProvinces() {
+      var that = this;
+      Object.keys(ChinaCoordinates).forEach(function(d) {
+        var lg = L.polygon(ChinaCoordinates[d].coordinates, {
+          color: "gray",
+          fillColor: "black",
+          fillOpacity: 0.1
+        }).addTo(that.map);
+        lg.on("click", function(e) {
+          console.log(d);
+          that.map.flyTo(e.latlng);
+        });
+      });
     }
   }
 };
